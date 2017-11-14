dbe9454031c101fc79f06e7418cc9cd751cd0672
ajomadlabs <ajojohn555@gmail.com>
2017-11-14 16:35:26 +0530

    Backend: Config Auth
    Added an auth config file for saving the
    facebook app credentials.
    
    Fixes #14 - Auth File

---
+++ chserver/app/config/auth.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    'facebookAuth': {
+
+        'clientID': '289285881583025',
+        'clientSecret': '1df56a2aac1962c91ce84b65b194378b',
+        'callBackURL': 'http://localhost:8081/auth/facebook/callback'
+
+    }
+    
+}

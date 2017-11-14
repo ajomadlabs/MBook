286991123be76af6b4215fc0f2910a9092de21d9
ajomadlabs <ajojohn555@gmail.com>
2017-11-14 14:42:06 +0530

    Backend: Modified the routes
    Added the login and signup routes.
    Added an additional function called
    isLoggedIn.
    
    Fixes #7 - Routes added

--- chserver/app/routes/index.js
+++ chserver/app/routes/index.js
@@ -1,9 +1,96 @@
-// Exporting the routes
+// Defining different Routes
 
-const signinRoute = require('./signin_route');
+const User = require('../models/user');
 
-module.exports = function (app, db) {
+module.exports = function(app, passport) {
 
-    signinRoute(app, db);
+    app.get('/', function(req, res) {
+
+        res.render('index.ejs');
+
+    });
+
+    app.get('/signup', function(req, res) {
+
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+
+    });
+
+    app.post('/signup', passport.authenticate('local-signup', {
+
+        successRedirect: '/',
+        failureRedirect: '/signup',
+        failureFlash: true
+
+    }));
+
+    app.get('/login', function(req, res) {
+
+        res.render('login.ejs', {message: req.flash('loginMessage')});
+
+    });
+
+    app.post('/login', passport.authenticate('local-login', {
+
+        successRedirect: '/profile',
+        failureRedirect: '/login',
+        failureFlash: true
+
+    }));
+
+    app.get('/profile', isLoggedIn, function(req, res) {
+
+        res.render('profile.ejs', {
+
+            user: req.user
+
+        });
+
+    });
+
+    app.get('/logout', function(req, res) {
+
+        req.logout();
+        res.redirect('/');
+
+    });
+
+    app.get('/:username/:password', function(req, res) {
+
+        const newUser = new User();
+
+        newUser.locallogin.username = req.params.username;
+        newUser.locallogin.password = req.params.password;
+
+        console.log(newUser.locallogin.username + " " + newUser.locallogin.password);
+
+        newUser.save(function(err) {
+
+            if (err) {
+
+                throw err;
+
+            }
+
+        });
+
+        res.send("Success !");
+
+    });
+};
+
+function isLoggedIn(req, res, next) {
+
+    if (req.isAuthenticated()) {
+
+        return next();
+
+    }
+
+    else {
+
+        res.redirect('/login');
+        
+    }
 
 }

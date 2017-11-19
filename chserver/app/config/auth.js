0634ebae946386826c6397897b7ce85f0a46c748
ajomadlabs <ajojohn555@gmail.com>
2017-11-19 21:28:53 +0530

    Frontend: Callback URL
    Changed the callback URL in auth.js file
    
    Fixes #25: Callback URL changes

--- chserver/app/config/auth.js
+++ chserver/app/config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
 
         'clientID': '22037346067-9o0vslvnhvot0vs1qa0oromvmppc976r.apps.googleusercontent.com',
         'clientSecret': 'OLgwtF3FDOX5TTKZ0i_LwXIA',
-        'callbackURL': 'http://localhost:8080/auth/google/callback'
+        'callbackURL': 'http://carehack-api.azurewebsites.net/auth/google/callback'
         
     }
         
